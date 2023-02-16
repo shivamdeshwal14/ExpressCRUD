79c1db67bfb6e628695959b908a59b7adb4f586b
SHIVAM DESHWAL <shivamdeshwal1414@gmail.com>
2023-02-16 17:50:54 +0530

    delete and display users

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express=require('express')
 const { resume } = require('./models/connections')
 const app=express()
+const engine=require('express-handlebars').engine   
 const db=require('./models/connections')
 app.use(express.json())
-// create user
+app.use(express.urlencoded({extended:true}))
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+// default page
+app.get("/",(req,res )=>{
+     res.render('home')
+})
+//    create user
 app.post("/adduser",(req,res)=>{
+    
     const user={name:req.body.name,email:req.body.email,city:req.body.city,phone:req.body.phone}
     let sql="INSERT INTO `employee` SET ?"
     db.query(sql,user,(err,result)=>{
@@ -12,7 +24,6 @@ app.post("/adduser",(req,res)=>{
         else
         res.json(result)
     })
-
 })
 // show user
 app.get("/showuser",(req,res)=>{
@@ -20,11 +31,15 @@ app.get("/showuser",(req,res)=>{
     db.query(sql,(err,result)=>{
         if(err) throw err
         else
-        res.json(result)
+        {
+            
+            res.render('show',({list:result}))
+        }
+        
     })
 })
 // show a particular use
-app.get('/showuser/:id',(req,res)=>{
+app. get('/showuser/:id',(req,res)=>{
    
     let sql="SELECT * FROM `employee`WHERE id="+req.params.id+""
     db.query(sql,(err,result)=>{
@@ -34,7 +49,8 @@ app.get('/showuser/:id',(req,res)=>{
     })
 })
 // DELETE
-app.delete("/deleteuser/:id",(req,res)=>{
+app.get("/deleteuser/:id",(req,res)=>{
+   
     let id=req.params.id
     let sql=`DELETE FROM employee WHERE id='${id}'`
     db.query(sql,(err,result)=>{
@@ -45,22 +61,23 @@ app.delete("/deleteuser/:id",(req,res)=>{
 })
 // update
 app.post('/updateuser/:id',(req,res)=>{
-    let id=req.params.id
-    const name =req.body.name
-    const email=req.body.email
-    const city=req.body.city
-    const phone=req.body.phone
+   console.log(req.params.id); 
+    // const name =req.body.name
+    // const email=req.body.email
+    // const city=req.body.city
+    // const phone=req.body.phone
 
-    let sql=`update employee SET name='${name}',phone='${phone}',email='${email}',city='${city}'`
-    db.query(sql,(err,result)=>{
-        if(err) throw err
-        else
-        res.json(result)
-    })
+    // let sql=`update employee SET name='${name}',phone='${phone}',email='${email}',city='${city}'`
+    // db.query(sql,(err,result)=>{
+    //     if(err) throw err
+    //     else
+    //     res.json(result)
+    // })
 })
+
 
 
 const PORT=process.env.PORT||3000
 app.listen(PORT,()=>{
     console.log(`SERVER IS RUNNING AT ${PORT}`)
-})
+}) 
